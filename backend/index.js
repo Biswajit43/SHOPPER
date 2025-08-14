f55fdf13e26a8bcf194c8a08a14762edde7b575e
Biswajit43 <143047694+Biswajit43@users.noreply.github.com>
2025-08-14 08:21:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use('/Images', express.static('upload/Images'))
 app.post('/upload', upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `https://shopper-backend-uolh.onrender.com/images/${req.file.filename}`
+        image_url: `https://shopper-backend-uolh.onrender.com/Images/${req.file.filename}`
     });
 });
 
