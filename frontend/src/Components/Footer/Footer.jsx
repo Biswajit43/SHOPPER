0eafaca2eb0fbe36a5b8679d3acf9e19f85aa09b
Biswajit43 <143047694+Biswajit43@users.noreply.github.com>
2025-08-21 10:37:47 +0530

    Update Footer.jsx

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import wpicon from '../Assets/whatsapp_icon.png'
-import fb from '../Assets/facebook.png'
-import mail from '../Assets/mail.png'
+import React from 'react';
+import wpicon from '../Assets/whatsapp_icon.png';
+import fb from '../Assets/facebook.png';
+import mail from '../Assets/mail.png';
 import logo from '../Assets/logo.png';
+
 const Footer = () => {
+    const menuItems = ["Company", "Products", "About Us", "Contact Us"];
+
     return (
         <div className='bg-[#e6f0ff] py-8'>
             <div className="flex flex-col gap-5">
-                <div className="flex flex-col  items-center">
-                    <img src={logo} />
-                    <h1 className='font-bold'>Shopper</h1>
+                
+                {/* Logo Section */}
+                <div className="flex flex-col items-center">
+                    <img src={logo} alt="Shopper Logo" className="h-12" />
+                    <h1 className='font-bold text-lg'>Shopper</h1>
                 </div>
-                <ul className='flex gap-4 justify-center font-bold'>
-                    <li>company</li>
-                    <li>Products</li>
-                    <li>About us</li>
-                    <li>contact us</li>
+
+                {/* Menu Items */}
+                <ul className='flex gap-6 justify-center font-bold text-gray-800 text-center'>
+                    {menuItems.map((item, index) => (
+                        <li key={index} className="cursor-pointer hover:text-blue-600 transition-colors duration-300">
+                            {item}
+                        </li>
+                    ))}
                 </ul>
+
+                {/* Social Media Icons */}
                 <div className="flex justify-center items-center gap-5 mt-4">
                     <img
                         src={wpicon}
@@ -34,9 +44,9 @@ const Footer = () => {
                         className="h-10 w-10 rounded-full p-1 cursor-pointer transition-transform duration-300 hover:scale-110"
                     />
                 </div>
-
             </div>
         </div>
-    )
-}
-export default Footer
+    );
+};
+
+export default Footer;
